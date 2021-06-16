a4bfd1c385ad236657b0c0922d0460b0540d921c
Yaniv bar <yanivb527@gmail.com>
2021-06-16 17:55:50 +0300

    commit 3.2 yaniv

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,91 +1,74 @@
-import { locService } from './services/loc.service.js'
-import { mapService } from './services/map.service.js'
+  import { locService } from './services/loc.service.js'
+  import { mapService } from './services/map.service.js'
 
-window.onload = onInit;
-window.onAddMarker = onAddMarker;
-window.onPanTo = onPanTo;
-window.onGetLocs = onGetLocs;
-window.onGetUserPos = onGetUserPos;
-window.onGo = onGo;
-window.onDelete = onDelete;
-window.onSearchPlace = onSearchLocation;
-window.onBuildLocation = onBuildLocation;
-window.onCloseModal = onCloseModal;
+  window.onload = onInit;
+  window.onAddMarker = onAddMarker;
+  window.onPanTo = onPanTo;
+  window.onGetLocs = onGetLocs;
+  window.onGetUserPos = onGetUserPos;
+  window.onBuildLocation = onBuildLocation;
+  window.onCloseModal = onCloseModal;
 
-var gLatLang;
+  var gLatLang;
 
+  function onInit() {
+      mapService.initMap()
+          .then(map => {
+              map.addListener("click", (mapsMouseEvent) => {
+                  gLatLang = {
+                      lat: mapsMouseEvent.latLng.lat(),
+                      lang: mapsMouseEvent.latLng.lng()
+                  };
+                  document.querySelector('.modal').hidden = false;
+              })
+          })
+          .catch((err) => console.log(err));
+  }
 
+  function onCloseModal() {
+      document.querySelector('.modal').hidden = true;
+  }
 
-function onInit() {
-    mapService.initMap()
-        .then(map => {
-            map.addListener("click", (mapsMouseEvent) => {
-                gLatLang = {
-                    lat: mapsMouseEvent.latLng.lat(),
-                    lang: mapsMouseEvent.latLng.lng()
-                };
-                onPanTo(gLatLang.lat, gLatLang.lang);
-                console.log(mapsMouseEvent.latLng.lat());
-                console.log(mapsMouseEvent.latLng.lng());
-            })
-        })
-        .catch((err) => console.log(err));
-}
+  function onBuildLocation(ev, elForm) {
+      ev.preventDefault();
+      document.querySelector('.modal').hidden = true;
+      const locName = elForm.querySelector('input').value;
+      locService.buildLocation(locName, gLatLang.lat, gLatLang.lang);
+  }
 
-// This function provides a Promise API to the callback-based-api of getCurrentPosition
-function getPosition() {
-    console.log('Getting Pos');
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
+  // This function provides a Promise API to the callback-based-api of getCurrentPosition
+  function getPosition() {
+      console.log('Getting Pos');
+      return new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+  }
 
-    })
-}
+  function onAddMarker() {
+      mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+  }
 
-function onAddMarker() {
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-}
-
-function onGetLocs() {
-    locService.getLocs()
-        .then(locs => {
-            document.querySelector('.locs').innerText = JSON.stringify(locs[0].formatted_address)
-
-
-        })
-}
-
-function onGetUserPos() {
-    getPosition()
-        .then(pos => {
-            mapService.showLocation(pos);
-            console.log('User position is:', pos.coords);
-            document.querySelector('.user-pos').innerText =
-                `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-        })
-        .catch(err => {
-            console.log('err!!!', err);
-        })
-}
-
-function onPanTo(lat, lang) {
-    mapService.panTo(lat, lang);
-}
-
-function onMyLocation() {
-    mapService.panToUserLocation();
-}
-
-function onGo() {
-    onPanTo(gLatLang.lat, gLatLang.lang);
-}
+  function onGetLocs() {
+      locService.getLocs()
+          .then(locs => {
+              document.querySelector('.locs').innerText = JSON.stringify(locs[0].formatted_address)
 
 
-function onDelete(el) {
-    console.log(el);
-    locService.deleteLocation(el.id)
-}
+          })
+  }
 
-function onSearchLocation() {
-    console.log('searching', document.getElementById('search-location').value);
-    mapservice.SearchPlace()
-}
+  function onGetUserPos() {
+      getPosition()
+          .then(pos => {
+              console.log('User position is:', pos.coords);
+              document.querySelector('.user-pos').innerText =
+                  `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+          })
+          .catch(err => {
+              console.log('err!!!', err);
+          })
+  }
+
+  function onPanTo(lat, lang) {
+      mapService.panTo(lat, lang);
+  }
